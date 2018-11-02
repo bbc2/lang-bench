f6023e14733bdd8f488c6bc88606c768ca99f399
Igor Valet <igor_valet@hotmail.com>
2018-11-02 22:56:37 +0100

    Add primes luajit support

---
+++ primes/lua-jit/compute.lua
@@ -0,0 +1,32 @@
+local PRIME_COUNT = assert(
+    os.getenv("PRIME_COUNT"),
+    "Please set PRIME_COUNT environment variable."
+)
+PRIME_COUNT = tonumber(PRIME_COUNT)
+
+local primes = {}
+local function is_prime(number)
+    for _, prime in ipairs(primes) do
+        if number % prime == 0 then
+            return false
+        end
+    end
+    return true
+end
+
+print("begin")
+io.stdout:flush()
+local number = 2
+while true do
+    if is_prime(number) then
+        print(number)
+        table.insert(primes, number)
+        if #primes == PRIME_COUNT then
+            break
+        end
+    end
+    number = number + 1
+end
+
+print("end")
+io.stdout:flush()
