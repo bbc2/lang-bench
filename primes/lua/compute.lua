9b36777638f4a3de58fc1bc5695c8cc059cfd415
Igor Valet <igor_valet@hotmail.com>
2018-11-03 10:21:15 +0100

    Cleaner loop

--- primes/lua/compute.lua
+++ primes/lua/compute.lua
@@ -18,13 +18,10 @@ print("begin")
 io.stdout:flush()
 
 local number = 2
-while true do
+while #primes < PRIME_COUNT do
     if is_prime(number) then
         print(number)
         table.insert(primes, number)
-        if #primes == PRIME_COUNT then
-            break
-        end
     end
     number = number + 1
 end
